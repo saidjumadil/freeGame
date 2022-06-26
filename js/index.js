e3ecb28980b8c78ea56a8f9b9bf1c249b64b62cb
Said Jumadil Akbar <s.jumadil.akbar@gmail.com>
2022-06-26 16:58:24 +0700

    tambahan comment

--- js/index.js
+++ js/index.js
@@ -39,7 +39,7 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=alph
                         <li>${data[i].platform}</li>
                         <li>${data[i].publisher}</li>
                     </ul>
-                    <h5><a href="/game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
+                    <h5><a href="./game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
                 </div>
             </div>
         </div>
@@ -74,7 +74,7 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=alph
                         <li>${data[i].platform}</li>
                         <li>${data[i].publisher}</li>
                     </ul>
-                    <h5><a href="/game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
+                    <h5><a href="./game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
                 </div>
             </div>
         </div>
@@ -100,7 +100,7 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=alph
             view += `<div class="product__sidebar__view__item set-bg" style="background-image: url(${thumbnail.toString()});">
                 <div class="ep">${data[i].genre}</div>
                 <div class="view"><i class="fa fa-gamepad"></i> ${data[i].developer}</div>
-                <h5><a href="/game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
+                <h5><a href="./game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
             </div>
             `
             if(i == 4){
@@ -124,7 +124,7 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=alph
             view += `<div class="product__sidebar__comment__item">
                 <div class="product__sidebar__comment__item__text">
                     <img src=${thumbnail.toString()} alt="" width="300" style="margin-bottom : 10px">
-                    <h5><a href="/game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
+                    <h5><a href="./game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
                     <ul>
                         <li>${data[i].platform}</li>
                         <li>${data[i].genre}</li>
@@ -154,7 +154,7 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/games?sort-by=alph
             view += `<div class="product__sidebar__comment__item">
                 <div class="product__sidebar__comment__item__text">
                     <img src=${thumbnail.toString()} alt="" width="300" style="margin-bottom : 10px">
-                    <h5><a href="/game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
+                    <h5><a href="./game-details.html?id=${data[i].id}">${data[i].title}</a></h5>
                     <ul>
                         <li>${data[i].platform}</li>
                         <li>${data[i].genre}</li>
